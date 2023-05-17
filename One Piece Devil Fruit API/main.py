99254282f0d79f74baf1436b8b97155a9c7e7ddd
manumafe98 <manumaxerapro@gmail.com>
2023-05-16 23:01:08 -0300

    Adding logic to get zoan and logia fruits aswell

--- One Piece Devil Fruit API/main.py
+++ One Piece Devil Fruit API/main.py
@@ -3,7 +3,11 @@ import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
+
+
+PARAMECIA_DIV = "/html/body/div[4]/div[3]/div[2]/main/div[3]/div[2]/div/div[2]"
+ZOAN_DIV = "/html/body/div[4]/div[3]/div[2]/main/div[3]/div[2]/div/div[4]"
+LOGIA_DIV = "/html/body/div[4]/div[3]/div[2]/main/div[3]/div[2]/div/div[6]"
 
 
 class DevilFruits:
@@ -11,17 +15,20 @@ class DevilFruits:
         self.chrome_options = Options()
         self.chrome_options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=self.chrome_options)
-        self.actions = ActionChains(self.driver)
         self.fruits = []
         self.fruits_dict = {}
 
-    def get_paramecia_fruits(self):
+    def get_fruits(self, div):
         self.driver.get("https://onepiece.fandom.com/wiki/Devil_Fruit")
-        div_element = self.driver.find_element(By.XPATH,
-                                               "/html/body/div[4]/div[3]/div[2]/main/div[3]/div[2]/div/div[2]")
+        time.sleep(1)
+        div_element = self.driver.find_element(By.XPATH, div)
         time.sleep(1)
         span_elements = div_element.find_elements(By.TAG_NAME, "span")
-        href_list = [span.find_element(By.CSS_SELECTOR, "div > a").get_attribute("href") for span in span_elements]
+        time.sleep(1)
+        excluded_links = ["https://onepiece.fandom.com/wiki/Vegapunk", "https://onepiece.fandom.com/wiki/Caesar_Clown",
+                          "https://onepiece.fandom.com/wiki/MADS"]
+        href_list = [span.find_element(By.CSS_SELECTOR, "div > a").get_attribute("href") for span in span_elements if
+                     span.find_element(By.CSS_SELECTOR, "div > a").get_attribute("href") not in excluded_links]
         return href_list
 
     def get_fruit_info(self, array):
@@ -62,6 +69,7 @@ class DevilFruits:
             try:
                 fruit_type_elem = self.driver.find_element(
                     By.XPATH, "/html/body/div[4]/div[3]/div[2]/main/div[3]/div[2]/div/aside/section/div[6]/div")
+            # Change try for if statement that if contains "chapter" try with div[7] instead
             except selenium.common.exceptions.NoSuchElementException:
                 fruit_type_elem = self.driver.find_element(
                     By.XPATH, "/html/body/div[4]/div[3]/div[2]/main/div[3]/div[2]/div/aside/section/div[7]/div")
@@ -81,12 +89,12 @@ class DevilFruits:
 
 
 devil_fruits = DevilFruits()
-fruit_list = devil_fruits.get_paramecia_fruits()
-devil_fruits.get_fruit_info(fruit_list)
+paramecia_list = devil_fruits.get_fruits(PARAMECIA_DIV)
+zoan_list = devil_fruits.get_fruits(ZOAN_DIV)
+logia_list = devil_fruits.get_fruits(LOGIA_DIV)
+final_list = paramecia_list + zoan_list + logia_list
+devil_fruits.get_fruit_info(final_list)
 print(devil_fruits.fruits)
 
-# TODO add a get_zoan_fruits method
-# TODO add a get_logia_fruits method
-# TODO check in this methods if the span has multiple anchor tags to skip or choose the last one
-# TODO in the zoan one skip when the only anchor tag == to mads
 # TODO create the flask get_all and get_specific_fruit with jsonify
+# TODO Fix type : "Chapter 921,Episode 912" in some zoan users

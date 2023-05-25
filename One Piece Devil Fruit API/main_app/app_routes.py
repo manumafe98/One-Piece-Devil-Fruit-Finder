2b6e5990b05d8d3151928fdbf8bbf66363b5a8fb
manumafe98 <manumaxerapro@gmail.com>
2023-05-25 15:52:08 -0300

    Make the search work and add the syling of the finded devil_fruit

--- One Piece Devil Fruit API/main_app/app_routes.py
+++ One Piece Devil Fruit API/main_app/app_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, url_for
 from form import SearchForm
 import requests
+import base64
 
 
 main = Blueprint("main", __name__)
@@ -10,10 +11,15 @@ main = Blueprint("main", __name__)
 def home():
     form = SearchForm()
     if form.validate_on_submit():
-        response = requests.get(f"localhost:5000/get/{form.search_element.data}")
+        response = requests.get(f"http://127.0.0.1:5001/get/{form.search_element.data}")
         if response.status_code == 200:
             devil_fruit = response.json()
-            image_url = devil_fruit["image_url"]
+            image_url = devil_fruit["fruit_img"]
             image_response = requests.get(image_url, stream=True)
-            return render_template("devil_fruit.html", devil_fruit=devil_fruit, image_url=image_response.raw)
+            if image_response.status_code == 200:
+                image_content = image_response.content
+                base64_image = base64.b64encode(image_content).decode('utf-8')
+                return render_template("devil_fruit.html", devil_fruit=devil_fruit, devil_fruit_img=base64_image)
     return render_template("index.html", form=form)
+
+# TODO add a check of form.search_element.data before making the api call and title case the devil_fruit
--- One Piece Devil Fruit API/main_app/form.py
+++ One Piece Devil Fruit API/main_app/form.py
@@ -4,4 +4,5 @@ from wtforms.validators import DataRequired
 
 
 class SearchForm(FlaskForm):
-    search_element = StringField(label="Search", validators=[DataRequired()])
+    search_element = StringField(label="Search_input", validators=[DataRequired()])
+    submit = SubmitField(label="Search")
